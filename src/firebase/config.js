cb82463582479b99d0ba6c4fd43fe228c1b4bfb8
Hoosier-Daddy <44615073+Hoosier-Daddy@users.noreply.github.com>
2021-04-21 00:58:26 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBrJaZL4PjLNDCiNLhSUdJdaNUNHoE2t8w",
-    authDomain: "firegram-image-gallery.firebaseapp.com",
-    projectId: "firegram-image-gallery",
-    storageBucket: "firegram-image-gallery.appspot.com",
-    messagingSenderId: "221457695784",
-    appId: "1:221457695784:web:065a1c208e95bec678a11d"
-}
+
 
 firebase.initializeApp(firebaseConfig);
 
