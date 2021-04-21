6db0aab2361307a8ed7734547085bed2e703face
Hoosier-Daddy <44615073+Hoosier-Daddy@users.noreply.github.com>
2021-04-22 01:59:16 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBrJaZL4PjLNDCiNLhSUdJdaNUNHoE2t8w",
+    authDomain: "firegram-image-gallery.firebaseapp.com",
+    projectId: "firegram-image-gallery",
+    storageBucket: "firegram-image-gallery.appspot.com",
+    messagingSenderId: "221457695784",
+    appId: "1:221457695784:web:065a1c208e95bec678a11d"
+}
 
 
 firebase.initializeApp(firebaseConfig);
@@ -11,8 +18,4 @@ const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
 const timeStamp = firebase.firestore.FieldValue.serverTimestamp;
 
-<<<<<<< HEAD
 export {projectStorage,projectFirestore,timeStamp};
-=======
-export default {projectStorage,projectFirestore};
->>>>>>> cb82463582479b99d0ba6c4fd43fe228c1b4bfb8
